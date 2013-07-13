10e435dc5622b2311d4bb23f2065be0fb17048aa
Geo Miller <geoffery.miller@gmail.com>
2013-07-13 19:34:45 -0400

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tallycode::Application.config.secret_key_base = 'f996d7e825a77b07819ff5866d1bebf77e9136d51b02a094d65d6733b32fe8c0dfd14cdb92d63702a58858afe86c6abc2d16ecc6063807d7a64eeb7e094de8e0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tallycode::Application.config.session_store :cookie_store, key: '_tallycode_session'
