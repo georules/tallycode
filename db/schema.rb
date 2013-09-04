88e51c1e96ae40d346f0bf7ff721647f84fb60c5
Tyler Doherty <tyler.john.doherty@gmail.com>
2013-09-04 17:42:31 -0400

    Started preliminary profiles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130714162906) do
+ActiveRecord::Schema.define(version: 20130904204715) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -26,9 +26,18 @@ ActiveRecord::Schema.define(version: 20130714162906) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
+    t.string   "firstname"
+    t.string   "lastname"
+    t.decimal  "age"
+    t.text     "website"
+    t.text     "github"
+    t.text     "twitter"
+    t.text     "mainlang"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
